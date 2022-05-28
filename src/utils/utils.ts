3c066e4a5d1a522316dadbcbdf961d349399d1dc
caue.palacio <caue.palacio@grupofleury.com.br>
2022-05-28 15:32:33 -0300

    feat: utils codes and data

---
+++ src/utils/utils.ts
@@ -0,0 +1,91 @@
+type Icons = {
+  id: string;
+  image: string;
+};
+
+export const diaNome: Array<string> = [
+  "Domingo",
+  "Segunda",
+  "Terça",
+  "Quarta",
+  "Quinta",
+  "Sexta",
+  "Sábado",
+];
+export const mesNome: Array<string> = [
+  "Jan",
+  "Fev",
+  "Mar",
+  "Abr",
+  "Mai",
+  "Jun",
+  "Jul",
+  "Ago",
+  "Set",
+  "Out",
+  "Nov",
+  "Dez",
+];
+
+export const icons: Array<Icons> = [
+  {
+    id: "11d",
+    image: "src/assets/icons/004-storm.png",
+  },
+  {
+    id: "01n",
+    image: "src/assets/icons/007-moon.png",
+  },
+  {
+    id: "01d",
+    image: "src/assets/icons/006-sun.png",
+  },
+  {
+    id: "02d",
+    image: "src/assets/icons/011-cloudy.png",
+  },
+  {
+    id: "02n",
+    image: "src/assets/icons/012-sky.png",
+  },
+  {
+    id: "13d",
+    image: "src/assets/icons/002-snowflake.png",
+  },
+  {
+    id: "04d",
+    image: "src/assets/icons/001-cloud.png",
+  },
+  {
+    id: "04n",
+    image: "src/assets/icons/001-cloud.png",
+  },
+  {
+    id: "03n",
+    image: "src/assets/icons/009-cloud-computing.png",
+  },
+  {
+    id: "03d",
+    image: "src/assets/icons/009-cloud-computing.png",
+  },
+  {
+    id: "09d",
+    image: "src/assets/icons/008-cloudy.png",
+  },
+  {
+    id: "10n",
+    image: "src/assets/icons/005-rain.png",
+  },
+  {
+    id: "10n",
+    image: "src/assets/icons/005-rain.png",
+  },
+  {
+    id: "50d",
+    image: "src/assets/icons/003-mist.png",
+  },
+  {
+    id: "50n",
+    image: "src/assets/icons/003-mist.png",
+  },
+];
