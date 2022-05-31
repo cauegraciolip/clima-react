24910d12553d9a517371ce3ce0ad6a8e2ceffb44
caue.palacio <caue.palacio@grupofleury.com.br>
2022-05-30 23:43:49 -0300

    feat: finished App.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Card } from "./components/Card";
-import { useState, useEffect } from "react";
 import { globalCss } from "@stitches/react";
-import axios from "axios";
 
-import { Box, Main, Section } from "./styles/stitchesStyles";
+import { Box, Section } from "./styles/stitchesStyles";
+
+import { useWeather } from "./provider/WeatherProvider";
 
 const globalStyles = globalCss({
   body: {
@@ -16,51 +16,25 @@ const globalStyles = globalCss({
 function App() {
   globalStyles();
 
-  const [location, setLocation] = useState<Boolean>(false);
-  const [weather, setWeather] = useState<any>();
-
-  const getMainWeather = async (lat: number, long: number): Promise<any> => {
-    let res = await axios.get(
-      "http://api.openweathermap.org/data/2.5/weather",
-      {
-        params: {
-          lat: lat,
-          lon: long,
-          appid: import.meta.env.VITE_APP_TOKEN,
-          lang: "PT",
-          units: "metric",
-        },
-      }
-    );
-    setWeather(res.data);
-  };
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position: GeolocationPosition) => {
-        getMainWeather(position.coords.latitude, position.coords.longitude);
-        setLocation(true);
-      }
-    );
-  }, []);
+  const { weather } = useWeather();
 
   return (
-    <Main>
-      <Section>
-        <Box>
-          <h4 style={{ textAlign: "center", color: "#f5f5f5", margin: 0 }}>
-            Previsão do Tempo
-          </h4>
-          {location && weather != undefined ? (
-            <Card {...weather} />
-          ) : (
-            <p style={{ color: "#ffffff" }}>
-              Você precisa autorizar sua localização
-            </p>
-          )}
-        </Box>
-      </Section>
-    </Main>
+    // <Main>
+    <Section>
+      <Box>
+        <h1 style={{ textAlign: "center", color: "#f5f5f5", margin: 0 }}>
+          Temperatura atual
+        </h1>
+        {weather != undefined ? (
+          <Card {...weather} />
+        ) : (
+          <p style={{ color: "#ffffff" }}>
+            Você precisa autorizar sua localização
+          </p>
+        )}
+      </Box>
+    </Section>
+    // </Main>
   );
 }
 
